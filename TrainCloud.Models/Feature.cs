d1be839f73aa54916513a6f1c3af376e4308b154
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-07-05 22:44:01 +0200

    Tenant - FeatureFlags

--- TrainCloud.Models/Feature.cs
+++ TrainCloud.Models/Feature.cs
@@ -1,10 +1,14 @@
 ﻿namespace TrainCloud.Models;
 
 [Flags]
-public enum Feature
+public enum Feature 
 {
     CarLists = 1,
     ForcedBrakings = 2,
-    FaultReports = 4,
-    Disturbances = 8
+    FaultReportsLocomotive = 4,
+    FaultReportsAvv = 8,
+    FaultReportsAtb = 16,
+    FaultReportsEtcs =32,
+    VehicleBook = 64,
+    Reports = 128
 }
