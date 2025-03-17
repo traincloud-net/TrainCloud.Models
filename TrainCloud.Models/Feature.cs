a86bef31027466c88c641e232d50b3b7476989b8
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2025-03-17 15:34:47 +0100

    FeatureMatrix

--- TrainCloud.Models/Feature.cs
+++ TrainCloud.Models/Feature.cs
@@ -3,12 +3,14 @@
 [Flags]
 public enum Feature 
 {
-    CarLists = 1,
-    ForcedBrakings = 2,
-    FaultReportsLocomotive = 4,
-    FaultReportsAvv = 8,
-    FaultReportsAtb = 16,
-    FaultReportsEtcs =32,
-    VehicleBook = 64,
-    Reports = 128
+    CarLists = 1, // TenantWgm & TenantOwner
+    DisturbanceForcedBrakings = 2, // TenantUser  (new&view)& TenantTfTraincee (new&view)+ TenantOwner
+    DisturbanceLocomotive = 4, // TenantUser (new&view)+ TenantOwner & ECM 1-4 & TenantTfTraincee (new&view)
+    DisturbanceAvv = 8, // TenantOwner & ECM 1-4 & TenantDamagedCarManager+ TenantWgm (new&view)
+    DisturbanceAtb = 16, // TenantUser  (new&view) + TenantOwner& TenantTfTraincee (new&view)
+    DisturbanceEtcs = 32, // TenantUser  (new&view)& TenantTfTraincee (new&view)+ TenantOwner
+    DisturbanceVehicleBook = 64, // TenantUser & TenantTfTraincee + TenantOwner & ECM 1-4
+    Reports = 128, // TenantOwner
+    Pzb = 265, // TenantOwner & TenantOperationalEvaluator
+    Ecm = 512 // TenantOwner & ECM 1-4
 }
