8bb632632bfb6eba76a9e8614816fafb49f7c19b
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2025-03-19 10:40:44 +0100

    Features

--- TrainCloud.Models/Feature.cs
+++ TrainCloud.Models/Feature.cs
@@ -1,16 +1,17 @@
 ﻿namespace TrainCloud.Models;
 
 [Flags]
-public enum Feature 
+public enum Feature : short
 {
-    CarLists = 1, // TenantWgm & TenantOwner
+    CarLists = 1, // TenantDispatcher & TenantWgm & TenantOwner
     DisturbanceForcedBrakings = 2, // TenantUser  (new&view)& TenantTfTraincee (new&view)+ TenantOwner
     DisturbanceLocomotive = 4, // TenantUser (new&view)+ TenantOwner & ECM 1-4 & TenantTfTraincee (new&view)
     DisturbanceAvv = 8, // TenantOwner & ECM 1-4 & TenantDamagedCarManager+ TenantWgm (new&view)
     DisturbanceAtb = 16, // TenantUser  (new&view) + TenantOwner& TenantTfTraincee (new&view)
     DisturbanceEtcs = 32, // TenantUser  (new&view)& TenantTfTraincee (new&view)+ TenantOwner
-    DisturbanceVehicleBook = 64, // TenantUser & TenantTfTraincee + TenantOwner & ECM 1-4
-    Reports = 128, // TenantOwner
-    Pzb = 265, // TenantOwner & TenantOperationalEvaluator
-    Ecm = 512 // TenantOwner & ECM 1-4
+    DisturbanceVehicleBook = 64, // TenantDispatcher & TenantUser & TenantTfTraincee + TenantOwner & ECM 1-4
+    DisturbanceBook = 128, // TenantDispatcher & TenantUser & TenantTfTraincee + TenantOwner & ECM 1-4
+    Reports = 256, // TenantOwner
+    Pzb = 512, // TenantOwner & TenantOperationalEvaluator
+    Ecm = 1024 // TenantOwner & ECM 1-4
 }
